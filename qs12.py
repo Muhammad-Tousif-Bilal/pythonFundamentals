68a27ceeab95f56c2c2cd2ff0c72552617dade2f
Muhammad-Tousif-Bilal <muhammadtousifbilal@gmail.com>
2026-08-25 22:29:25 +0500

    Qs 12 & 13 completed

---
+++ qs12.py
@@ -0,0 +1,5 @@
+# Print all even numbers between 1 and N
+n = 26
+for i in range(1, n+1):
+    if(i%2 == 0):
+        print(i)
---
+++ qs13.py
@@ -0,0 +1,4 @@
+# Print multiplication table of a number
+n = 2
+for i in range(1, 11):
+    print(f"{n} x {i} = {n*i}")
