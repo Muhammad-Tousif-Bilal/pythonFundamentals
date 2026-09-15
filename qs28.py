0d66d4b6af245c7451f9a0a77f83a7aac69b314b
Muhammad-Tousif-Bilal <muhammadtousifbilal@gmail.com>
2026-09-15 20:03:19 +0500

    Qs 28 completed

--- qs28.py
+++ qs28.py
@@ -1,1 +1,23 @@
 # Add two 2D matrices 
+
+matrix1 = [ 
+    [1, 2, 3],  
+    [4, 5, 6]   
+]
+
+matrix2 = [
+            [1, 2, 3],   
+            [1, 1, 1]    
+]
+TwoD = []
+
+for i in range(len(matrix1)):
+    add = []
+    for j in range(len(matrix2[i])):
+        sum = matrix1[i][j] + matrix2[i][j]
+        add.append(sum)
+    TwoD.append(add)
+
+print(TwoD)
+
+    
