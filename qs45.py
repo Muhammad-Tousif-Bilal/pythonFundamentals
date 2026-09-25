9ebc934abbc403c98a6450e9522acd54fb0bbc78
Muhammad-Tousif-Bilal <muhammadtousifbilal@gmail.com>
2026-09-25 19:33:33 +0500

    Qs 45 completed

--- qs45.py
+++ qs45.py
@@ -1,1 +1,10 @@
 #  Function to check palindrome 
+n = "madaM"
+def palindrome(text):
+    lwr_text = text[::-1].lower()
+    if(lwr_text == text.lower()):
+        print("It is palindrome")
+    else:
+        print("It is not palindrome")
+
+palindrome(n)
